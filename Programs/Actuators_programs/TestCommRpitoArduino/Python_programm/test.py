fce8d09a8468a40b0fcf76cead4c500aa7ebc607
corentindpt <57626460+corentindpt@users.noreply.github.com>
2020-04-24 17:19:21 +0200

    update programme test.py for using 3 variables

--- Programs/Actuators_programs/TestCommRpitoArduino/Python_programm/test.py
+++ Programs/Actuators_programs/TestCommRpitoArduino/Python_programm/test.py
@@ -4,9 +4,11 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 print("CTRL + C pour arrêter")
 
 while True:
-    action = input("Souhaitez-vous simuler la présence d'une personne connue ou inconnue ? (Connue = 1, inconnue = 0)")
+    action = input("Souhaitez-vous simuler la présence d'une personne connue ou inconnue ou personne ? (Connue = 1, inconnue = 0, personne = 2)")
     print(action)
     if action == '1':
         ser.write(b'1')
-    else:
+    elif action == '0':
         ser.write(b'0')
+    else:
+        ser.write(b'2')
